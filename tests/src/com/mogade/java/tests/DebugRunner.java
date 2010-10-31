2199821b1c557ce46b05c182751f8c5e9966065d
Brian Nesbitt <sport11can@gmail.com>
2010-10-31 09:20:05 -0400

    Added proxy support to the DebugRunner to see the requests in fiddler

--- tests/src/com/mogade/java/tests/DebugRunner.java
+++ tests/src/com/mogade/java/tests/DebugRunner.java
@@ -8,6 +8,11 @@ public class DebugRunner
 {
    public static void main(String[] args)
    {
+      if (args.length == 2)
+      {
+         System.setProperty("http.proxyHost", args[0]);
+         System.setProperty("http.proxyPort", args[1]);
+      }
       org.junit.runner.JUnitCore.runClasses(TestMogade.class);
       org.junit.runner.JUnitCore.runClasses(TestScore.class);
       org.junit.runner.JUnitCore.runClasses(TestUtility.class);
